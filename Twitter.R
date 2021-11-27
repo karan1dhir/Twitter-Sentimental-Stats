b6e95229ae13360f55511013ec100edabb7b4290
karan1dhir <karandhir64@yahoo.co.in>
2021-11-26 20:33:43 -0700

    merging and code cleaning

--- Twitter.R
+++ Twitter.R
@@ -8,11 +8,13 @@ library(plyr)
 library(dismo)
 library(maps)
 library(ggmap)
+library(wordcloud)
+library(RColorBrewer)
 
 
 
 # Setting up the credentials
-TwitterClient <- function(searchString,no_of_tweets){
+TwitterClient <- function(search_string,no_of_tweets){
   
   api_key = as.character("mpJ47SSV27W875Coe1VRbI6hs")
   api_secret = as.character("1ukBRBBESV5WZqkAsA0vqeXeUJzKmOQVvmahgfglkeX5qCXluC")
@@ -23,14 +25,14 @@ TwitterClient <- function(searchString,no_of_tweets){
   
   inital_date <- Sys.Date() - 60
   current_date <- Sys.Date(); 
-  MTweets <- searchTwitter(searchString,n = no_of_tweets,since =as.character(inital_date),until=as.character(current_date))
+  MTweets <- searchTwitter(search_string,n = no_of_tweets,since =as.character(inital_date),until=as.character(current_date))
   df <- do.call("rbind",lapply(MTweets,as.data.frame))
   return(list(t_str= MTweets,t_df = df))
 }
 
 ## Cleaning the tweets 
 
-clean.tweets = function(tweet)
+clean_tweets = function(tweet)
 {
   # convert to lower case
   tweet = tolower(tweet)
@@ -59,24 +61,41 @@ clean.tweets = function(tweet)
   tweet = gsub('\\d+', '', tweet)
   return(tweet)
 }
-
-
+# combining the information extracted from the user and the tweets.
+combine_tweet_and_userInfo <- function(tweet_obj){
+ 
+  user_info <- lookupUsers(tweet_obj$t_df$screenName)
+  user_df <- twListToDF(user_info)
+  
+  tweet_df <- tweet_obj$t_df %>% 
+    full_join(user_df,by = c('screenName')) %>% dplyr::select(-c(longitude,latitude)) %>%
+    dplyr::select(text,favorited,favoriteCount,created.x,statusSource,retweetCount,retweetCount,description,name,location)
+  
+  return (tweet_df)
+   
+} 
 
 ## API call-out to get all the tweets belonging to a particular hash-tag.
 searchString <- "Trump"
 no_of_tweets <- 3500
 tweet_obj <- TwitterClient(searchString,no_of_tweets)
-tweets.txt <- sapply(tweet_obj$t_str, function(t)t$getText())
+
+## Get the user information of the tweets.
+
+tweet_df <- combine_tweet_and_userInfo(tweet_obj)
 
 
-filtered_tweets <- clean.tweets(tweets.txt)
-#View(twitter_results)
+## Clean all the tweets by removing all the punctuation, stop words, links http, tabs, blank spaces.
+filter_tweets <- clean_tweets(tweet_df$text)  
+
+
+tweet_df <- tweet_df %>% 
+  dplyr::select(-c(text))%>% 
+  mutate(text = filter_tweets)
 
 
 
-userInfo <- lookupUsers(twitter_results$screenName)
-userFrame <- twListToDF(userInfo)
-locatedUsers <- !is.na(userFrame$location)
+
 
 
 
